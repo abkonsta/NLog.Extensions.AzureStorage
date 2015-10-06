f3b4eccc608b18ed31e7c0cdc7258c066977d3ab
Andre Konstantinov <Andre.Konstantinov@infogroup.com>
2015-10-06 12:24:48 -0500

    Allow CloudStorageAccount.Parse(connectionString) to fail during the initial application load (to allow to configure it later in code)

--- NLog.Extensions.AzureTableStorage/TableStorageManager.cs
+++ NLog.Extensions.AzureTableStorage/TableStorageManager.cs
@@ -26,7 +26,13 @@ namespace NLog.Extensions.AzureTableStorage
         #region Constructors
         public TableStorageManager(string connectionString, string tableName)
         {
-            Initialize(connectionString, tableName);
+            // the constructor for the table storage manager only initializes the properties
+            // the actual connection is deferred till we need it
+
+            // one of the reasons for doing this is to avoid the startup errors because the connection
+            // string, if it is environment-dependent, may not yet be valid. watch the trace logs ;)
+            _connectionString = connectionString;
+            _tableName = tableName;
         }
         #endregion Constructors
 
@@ -39,9 +45,19 @@ namespace NLog.Extensions.AzureTableStorage
             if (string.IsNullOrWhiteSpace(tableName))
                 return;
 
-            // if connection string or table name have changed...
+            // if connection string or table name have changed, invalidate
+            // the existing connection
             if (_connectionString != connectionString || _tableName != tableName)
             {
+                Trace.TraceInformation("NLog.Extensions.AzureTableStorage connection information has changed.");
+                Trace.TraceInformation("NLog.Extensions.AzureTableStorage will (re)initialize.");
+                _cloudTable = null;
+            }
+
+            // _cloudTable may be null for two reasons: 1) it hasn't been initialized yet and 2) it was
+            // cleared because connection information changed
+            if(_cloudTable == null)
+            { 
                 // re-initialize the storage manager. the target will now log to the newly specified
                 // storage account and table
                 Initialize(connectionString, tableName);
@@ -53,17 +69,25 @@ namespace NLog.Extensions.AzureTableStorage
             _connectionString = connectionString;
             _tableName = tableName;
 
-            // get the storage account
-            var storageAccount = CloudStorageAccount.Parse(connectionString);
-            Trace.TraceWarning($"NLog.Extensions.AzureTableStorage will log to {storageAccount.TableEndpoint.AbsoluteUri}");
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                Trace.TraceInformation("NLog.Extensions.AzureTableStorage does not have a connection string.");
+                Trace.TraceInformation("You will need to specify it in NLog.config, or assign one in code.");
+                return;
+            }
 
             // it is possible that at this stage we do not yet have a storage account; for example
             // if we defer the NLog target initialization till Global.asax.cs
 
-            // in this case, we will just let this silently fail. it will get re-initialized
-            // once a valid storage account connection string is available
+            // in this case, we will just let this fail (and log the exception, of course). 
+            // you will need to make sure the target gets reinitialized in code, once a valid storage 
+            // account connection string is available
             try
             {
+                // get the storage account
+                var storageAccount = CloudStorageAccount.Parse(connectionString);
+                Trace.TraceInformation($"NLog.Extensions.AzureTableStorage will log to {storageAccount.TableEndpoint.AbsoluteUri}");
+
                 // Create the table client.
                 var tableClient = storageAccount.CreateCloudTableClient();
 
@@ -99,6 +123,7 @@ namespace NLog.Extensions.AzureTableStorage
             if (_cloudTable == null)
             {
                 Trace.TraceWarning("NLog.Extensions.AzureTableStorage logging was attempted but the target is not initialized.");
+                Trace.TraceWarning("You will not capture any NLog messages until the target initializes successfully.");
                 Trace.TraceWarning("This could be due to a missing connection string or an error during the target initialization.");
                 Trace.TraceWarning("Please check the log for previous error messages!");
 
